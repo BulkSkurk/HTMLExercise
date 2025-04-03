437228db8ea89e6d7d9fa4d1a2120abafc816fc2
BulkSkurk <adamtimra@hotmail.com>
2025-04-03 20:23:34 +0200

    add todays date

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 'use strict';
 
+var currentTime = new Date().toISOString().slice(0, 10);
+
+document.querySelector('#todays-date').textContent = `Current Date: ${currentTime}`;
+
 document.querySelectorAll('.read-more-btn').forEach((button) => {
   button.addEventListener('click', function () {
     const hiddenText = button.closest('.newsarticle').querySelector('.hidden-text');
