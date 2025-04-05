494ead715f6e2ee7b1baaf9ea484b27b47e57058
BulkSkurk <adamtimra@hotmail.com>
2025-04-05 21:00:07 +0200

    reactivate read-more button and add featurette

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 'use strict';
 
-var currentTime = new Date().toISOString().slice(0, 10);
-
-document.querySelector('#todays-date').textContent = `Current Date: ${currentTime}`;
-
 document.querySelectorAll('.read-more-btn').forEach((button) => {
   button.addEventListener('click', function () {
-    const hiddenText = button.closest('.newsarticle').querySelector('.hidden-text');
-    hiddenText.classList.toggle('hidden');
+    const hiddenText = button.closest('.col-md-7').querySelector('.hidden-text');
+    hiddenText.classList.toggle('show');
   });
 });
