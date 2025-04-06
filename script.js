4a2c39b88fae5cee63e670620cfe4dc02eb71873
BulkSkurk <adamtimra@hotmail.com>
2025-04-06 15:15:27 +0200

    add read more and read less text to buttons

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ document.querySelectorAll('.read-more-btn').forEach((button) => {
   button.addEventListener('click', function () {
     const hiddenText = button.closest('.col-md-7').querySelector('.hidden-text');
     hiddenText.classList.toggle('show');
+    var valueToSet = button.innerHTML == 'Read More!' ? 'Read Less!' : 'Read More!';
+    button.innerHTML = valueToSet;
   });
 });
